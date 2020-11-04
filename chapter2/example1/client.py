c623cbead653b74f68baa2985a55aef1df77087e
杨恺 <yangkai02@qutoutiao.net>
2020-11-04 11:34:14 +0800

    [feat]sender buffer, receive buffer

--- chapter2/example1/client.py
+++ chapter2/example1/client.py
@@ -4,14 +4,12 @@
 import socket
 import time
 
-
-
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(("localhost", 8081))  # 连接服务器
 count = 0
 while True:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("localhost", 8081))  # 连接服务器
     count += 1
-    sock.sendall("hello")  # 将消息输出到发送缓冲 send buffer
+    sock.sendall("hello"*1000)  # 将消息输出到发送缓冲 send buffer, 发送缓冲区大小约为512K, 因此只能连续发110次左右的请求就会停
     print(count)
     # print sock.recv(1024)  # 从接收缓冲 recv buffer 中读响应
-    sock.close()  # 关闭套接字
+sock.close()  # 关闭套接字
--- chapter2/example1/server.py
+++ chapter2/example1/server.py
@@ -6,9 +6,11 @@ import socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(("localhost", 8081))
 sock.listen(1)  # 监听客户端连接
+conn, addr = sock.accept()  # 接收一个客户端连接
+print(addr)
+
 while True:
-    conn, addr = sock.accept()  # 接收一个客户端连接
-    print(addr)
-    # print conn.recv(1024)  # 从接收缓冲读消息 recv buffer
+    # print conn.recv(50000000)  # 从接收缓冲读消息 recv buffer
     # conn.sendall("world")  # 将响应发送到发送缓冲 send buffer
-    conn.close()  # 关闭连接
+    time.sleep(100)
+conn.close()  # 关闭连接
